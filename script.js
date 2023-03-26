f9b2ad83f54d355bf33892e0f23daf343423a8f9
Nurudeen <abdulmajeednurudeen48@gmail.com>
2023-03-26 22:28:26 +0000

    I added the function to get the computer choice and another function to play the game once

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+const choice = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+  const result = choice[Math.floor(Math.random() * choice.length)];
+  return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+  const playerSelection = prompt('Enter you choice');
+  const computerSelection = getComputerChoice();
+ console.log(playRound(playerSelection, computerSelection));
