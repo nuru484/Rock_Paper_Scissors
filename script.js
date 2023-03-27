1f5c9b24f481b07784b031df18b8cadebc06cbb8
Nurudeen <abdulmajeednurudeen48@gmail.com>
2023-03-27 15:11:24 +0000

    I added a some if statements to track the score of both the player aand the computer

--- script.js
+++ script.js
@@ -20,12 +20,19 @@ function playRound(playerSelection, computerSelection) {
 }
   
 function game(){
+  let playerScore = 0;
+  let computerScore = 0;
   for (let i = 0; i <= 5; i++){
     const playerSelection = prompt('Enter you choice');
     const computerSelection = getComputerChoice();
    console.log(playRound(playerSelection, computerSelection));
-  }
-      
-  
+   const result = playRound(playerSelection, computerSelection);
+   if (result == 'You win! ' + playerSelection + ' beats ' + computerSelection){
+   playerScore++;
+   }else if ( result == "You lose! " + computerSelection + " beats " + playerSelection){
+    computerScore++;
+   }
+   console.log(`Final score: Player ${playerScore} - ${computerScore} Computer`);
+}
 }
  console.log(game());
