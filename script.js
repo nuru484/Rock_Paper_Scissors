6eb5746246f00d1301cc7d23b453f5a7a6b6690f
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2023-05-28 18:02:41 -0800

    Added UI to the game

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const rockk = document.querySelector('#rock');
+const paperr = document.querySelector('#paper');
+const scissorss = document.querySelector('#scissors');
+const resultsWindow1 = document.querySelector('.results1');
+const resultsWindow2 = document.querySelector('.results2');
+
 const choice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
@@ -18,21 +24,50 @@ function playRound(playerSelection, computerSelection) {
     return "You lose! " + computerSelection + " beats " + playerSelection;
   }
 }
-  
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = '';
+
+rockk.addEventListener('click', function(){
+  playerSelection = 'rock';
+  game();
+});
+
+paperr.addEventListener('click', function(){
+  playerSelection = 'paper';
+  game();
+});
+
+scissorss.addEventListener('click', function(){
+  playerSelection = 'scissors';
+  game();
+});
+
 function game(){
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i <= 5; i++){
-    const playerSelection = prompt('Enter you choice');
     const computerSelection = getComputerChoice();
-   console.log(playRound(playerSelection, computerSelection));
    const result = playRound(playerSelection, computerSelection);
    if (result == 'You win! ' + playerSelection + ' beats ' + computerSelection){
    playerScore++;
    }else if ( result == "You lose! " + computerSelection + " beats " + playerSelection){
     computerScore++;
    }
-   console.log(`Final score: Player ${playerScore} - ${computerScore} Computer`);
+   resultsWindow2.textContent = result;
+ resultsWindow1.textContent=(`Scores: Player ${playerScore} - ${computerScore} Computer`);
+
+ if(playerScore === 5){
+  const finalScoreElement = document.querySelector('h1');
+  finalScoreElement.style.textAlign = "center";
+  finalScoreElement.textContent = `The player win, with a score of 5 against computer with a score of ${computerScore}`;
+  document.open();
+  document.write(finalScoreElement.outerHTML);
+  document.close();
+} else if (computerScore === 5){
+  const finalScoreElement = document.querySelector('h1');
+  finalScoreElement.classList.add('centered');
+  finalScoreElement.style.textAlign = "center";
+  finalScoreElement.textContent = `The Computer won, with a score of 5 against player with a score of ${playerScore}`;
+  document.open();
+  document.write(finalScoreElement.outerHTML);
+  document.close();
 }
 }
- console.log(game());
